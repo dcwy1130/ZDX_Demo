015d33af66162032cbb0eb26ae913decd13b4ec5
dcwy1130 <thisis_java@163.com>
2016-04-20 11:29:40 +0800

    Initial Commit

---
+++ ZDX_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZDX_Demo
+//
+//  Created by 张德雄 on 16/4/20.
+//  Copyright © 2016年 GroupFly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
